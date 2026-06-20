8992527d500ca42e10c6a8c22e6bf180352c540b
aesha <aeshasangani29@gmail.com>
2026-06-20 09:56:58 +0530

    feat: added risk scoring to alerts

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ app = FastAPI()
 
 alerts = []
 
+def get_risk_score(severity):
+
+    if severity == "high":
+        return "High"
+
+    elif severity == "medium":
+        return "Medium"
+
+    return "Low"
+
+
 @app.get("/")
 def home():
     return {"status": "SOAR Running"}
+
 
 @app.post("/alert")
 def alert(data: dict):
@@ -18,12 +30,18 @@ def alert(data: dict):
         "alert_type": data.get("alert_type")
     }
 
+    # Add risk score
+    normalized_alert["risk_score"] = get_risk_score(
+        normalized_alert["severity"]
+    )
+
     alerts.append(normalized_alert)
 
     return {
         "message": "Alert Saved",
         "normalized_alert": normalized_alert
     }
+
 
 @app.get("/alerts")
 def get_alerts():
