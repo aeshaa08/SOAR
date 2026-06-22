65b29ad40fed85dd5259fc45985a19793c96d31d
aesha <aeshasangani29@gmail.com>
2026-06-22 18:48:21 +0530

    OTA firmware update

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from datetime import datetime
 
 app = FastAPI()
 
-# File names
+
 FIRMWARE_FILE = "firmware.bin"
 SIGNATURE_FILE = "firmware.sig"
 
-# Firmware details
+
 firmware_info = {
     "version": "1.0.0",
     "release_date": str(datetime.now()),
@@ -17,11 +17,9 @@ firmware_info = {
     "signature_file": SIGNATURE_FILE
 }
 
-# Security logs
 security_logs = []
 
 
-# Generate SHA-256 hash
 def generate_hash(filename):
 
     if not os.path.exists(filename):
@@ -33,7 +31,6 @@ def generate_hash(filename):
     return hashlib.sha256(data).hexdigest()
 
 
-# Home page
 @app.get("/")
 def home():
     return {
@@ -41,7 +38,6 @@ def home():
     }
 
 
-# Check latest firmware
 @app.get("/firmware/latest")
 def get_latest_firmware():
 
@@ -60,7 +56,6 @@ def get_latest_firmware():
     }
 
 
-# Download firmware information
 @app.get("/firmware/download")
 def download_firmware():
 
@@ -75,8 +70,6 @@ def download_firmware():
         "hash": generate_hash(FIRMWARE_FILE)
     }
 
-
-# Add security event logs
 @app.post("/security/log")
 def add_security_log(data: dict):
 
@@ -94,8 +87,6 @@ def add_security_log(data: dict):
         "log": log
     }
 
-
-# View all security logs
 @app.get("/security/logs")
 def get_logs():
     return security_logs
